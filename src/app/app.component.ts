54efb86f2026338ce2ca58c67caac031e5c09325
Angelo Amadora <amadora.angelo@gmail.com>
2018-02-12 15:39:14 +0800

    added NewhomePage
    temporaritly changed root to NewhomePage for dev purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {NewhomePage} from '../pages/newhome/newhome'
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // change back to 
+  //rootPage:any = HomePage;
+  rootPage:any = NewhomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
