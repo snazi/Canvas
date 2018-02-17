fb3350cf715ae53f359cd3a86e786a1d9f63d6dc
Angelo Amadora <amadora.angelo@gmail.com>
2018-02-17 15:19:04 +0800

    revised layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
 
   @ViewChild(Nav) nav: Nav;
 
-  rootPage:any = HomePage;
+  rootPage:any = NewhomePage;
 
   pages: Array<{title: string, component: any, imgSource: string}> ;
 
